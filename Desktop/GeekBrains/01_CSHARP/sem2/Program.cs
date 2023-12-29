a6244ddbfab28b37d6677710d52d2c09aa248ccd
Anastasiya <ayrodin@gmail.com>
2023-12-29 23:20:25 +0700

    ДЗ

--- Desktop/GeekBrains/01_CSHARP/sem2/Program.cs
+++ Desktop/GeekBrains/01_CSHARP/sem2/Program.cs
@@ -72,24 +72,24 @@
 // 7812 => 8
 // 91 => Третьей цифры нет
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int copyNumber = number;
-if (number < 100) // Число не трехзначное
-{
-    Console.WriteLine("Третьей цифры нет :)");
-}
-else // Число минимум трехзначное 
-{
-    int digitCount = 0; // Количество цифр, которое нужно убрать 
-    int digit = 0; // Третья цифра справа
-    while (digitCount < 2) // Отбрасываем 2 последние цифры
-    {
-        number /= 10;
-        digitCount++; // digitCount = digitCount + 1
-    }
-    Console.WriteLine($"Третья цифра справа от числа {copyNumber} = {number % 10}");
-}
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int copyNumber = number;
+// if (number < 100) // Число не трехзначное
+// {
+//     Console.WriteLine("Третьей цифры нет :)");
+// }
+// else // Число минимум трехзначное 
+// {
+//     int digitCount = 0; // Количество цифр, которое нужно убрать 
+//     int digit = 0; // Третья цифра справа
+//     while (digitCount < 2) // Отбрасываем 2 последние цифры
+//     {
+//         number /= 10;
+//         digitCount++; // digitCount = digitCount + 1
+//     }
+//     Console.WriteLine($"Третья цифра справа от числа {copyNumber} = {number % 10}");
+// }
 
 // int number = 123;
 // Console.WriteLine(number / 100 % 10);
@@ -102,3 +102,68 @@ else // Число минимум трехзначное
 // {
 //     Console.WriteLine($"{a} кратно 7");
 // }
+
+//  Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+// Console.Write("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 7 == 0 && a % 23 == 0)
+// {
+// Console.WriteLine("да");
+// }
+// else
+// {
+// Console.WriteLine("нет");
+// }
+
+// Console.Write("Введите координаты точки X и Y через пробел: ");
+// string[] coordinates = Console.ReadLine().Split(' ');
+// int x = Convert.ToInt32(coordinates[0]);
+// int y = Convert.ToInt32(coordinates[1]);
+// if (x > 0 && y > 0)
+// Console.WriteLine("1");
+// else if (x < 0 && y > 0)
+// Console.WriteLine("2");
+// else if (x < 0 && y < 0)
+// Console.WriteLine("3");
+// else if (x > 0 && y < 0)
+// Console.WriteLine("4");
+// else
+// Console.WriteLine("Точка находится на оси координат");
+
+// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Console.Write("Введите число из отрезка [10, 99]: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine(maxDigit);
+
+// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+// Console.Write("Введите натуральное число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (N < 10)
+// {
+// Console.WriteLine(N);
+// }
+// else
+// {
+// while (N > 0);
+
+// int currentDigit = N % 10;
+// N /= 10;
+// if (N > 0)
+// {
+// Console.Write(currentDigit + ",");
+// }
+// else
+// {
+// Console.WriteLine(currentDigit);
+// }
+// }
+
+
+
+
