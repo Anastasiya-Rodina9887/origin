b3d15154c27b15bcdbcf2b76324b03e945a83626
Anastasiya <ayrodin@gmail.com>
2024-01-12 00:44:10 +0700

    Решили первую задачу

---
+++ OneDrive/Рабочий стол/GeekBrains/01_CSHARP/sem3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве. Программа 
+// должна выдать ответ: Да/Нет.
+
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да 
+using System.Runtime.InteropServices;
+
+int[] array = {11, 22, 33, 44, 55, 66, 77 };
+Console.Write("Введите число для поиска в массиве: ");
+int numberForSearch = Convert.ToInt32(Console.Readline());
+bool isFound = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (numberForSearch == array [i]);
+    {
+        isFound = true;
+        break;
+    }
+}
+if (isFound)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
