2331e6715e42ec7b73dfb709ad63bf7a9a0738a9
Anastasiya <ayrodin@gmail.com>
2024-01-12 01:17:11 +0700

    Решили вторую задачу

--- OneDrive/Рабочий стол/GeekBrains/01_CSHARP/sem3/Program.cs
+++ OneDrive/Рабочий стол/GeekBrains/01_CSHARP/sem3/Program.cs
@@ -29,17 +29,47 @@
 //     Console.WriteLine(" Нет ");
 // }
 
-int[] array = {11, 22, 33, 44, 55, 66, 77 };
-Console.Write("Введите число для поиска в массиве: ");
-int numberForSearch = Convert.ToInt32(Console.ReadLine());
-string isFound = "не найден";
+// int[] array = {11, 22, 33, 44, 55, 66, 77 };
+// Console.Write("Введите число для поиска в массиве: ");
+// int numberForSearch = Convert.ToInt32(Console.ReadLine());
+// string isFound = "не найден";
 
-for (int i = 0; i < array.Length; i++)
-{
-    if (numberForSearch == array[i])
-    {
-        isFound = "найден";
-        break;
-    }
-}
-Console.WriteLine($"Элемент {numberForSearch}: {isFound} ");
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (numberForSearch == array[i])
+//     {
+//         isFound = "найден";
+//         break;
+//     }
+// }
+// Console.WriteLine($"Элемент {numberForSearch}: {isFound} ");
+
+
+// Задайте массив из 10 элементов, заполненный числами из 
+// промежутка [-10, 10]. Замените отрицательные элементы на 
+// положительные, а положительные на отрицательные.
+
+// Пример
+// [1 -5 6]
+// => [-1 5 -6]
+
+// int[] array = {1, -2, 3, 4, 5, -1, -7, -9, 10, -5 };
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] *= -1;  // array[i] = array[i] * -1;
+//     Console.Write(array[i] + "\t");
+// }
+
+
+// int[] array = {1, -2, 3, 4, 5, -1, -7, -9, 10, -5 };
+// Console.WriteLine($"Массив ДО: [ {string.Join("; ", array)} ]");
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] *= -1;  // array[i] = array[i] * -1;
+// }
+// Console.WriteLine($"Массив ПОСЛЕ: [ {string.Join("; ", array)} ]");
+
+
+
+
+
