54e644471f97aa281397f99789ccbf16881c8f7e
Anastasiya <ayrodin@gmail.com>
2024-01-12 00:55:39 +0700

    Решили первую задачу

--- OneDrive/Рабочий стол/GeekBrains/01_CSHARP/sem3/Program.cs
+++ OneDrive/Рабочий стол/GeekBrains/01_CSHARP/sem3/Program.cs
@@ -5,26 +5,41 @@
 // Примеры
 // [1 3 4 19 3], 8 => Нет
 // [-4 3 4 1], 3 => Да 
-using System.Runtime.InteropServices;
+// using System.Runtime.InteropServices;
+
+// int[] array = {11, 22, 33, 44, 55, 66, 77 };
+// Console.Write("Введите число для поиска в массиве: ");
+// int numberForSearch = Convert.ToInt32(Console.ReadLine());
+// bool isFound = false;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (numberForSearch == array [i]);
+//     {
+//         isFound = true;
+//         break;
+//     }
+// }
+// if (isFound)
+// {
+//     Console.WriteLine(" Да ");
+// }
+// else
+// {
+//     Console.WriteLine(" Нет ");
+// }
 
 int[] array = {11, 22, 33, 44, 55, 66, 77 };
 Console.Write("Введите число для поиска в массиве: ");
-int numberForSearch = Convert.ToInt32(Console.Readline());
-bool isFound = false;
+int numberForSearch = Convert.ToInt32(Console.ReadLine());
+string isFound = "не найден";
 
 for (int i = 0; i < array.Length; i++)
 {
-    if (numberForSearch == array [i]);
+    if (numberForSearch == array[i])
     {
-        isFound = true;
+        isFound = "найден";
         break;
     }
 }
-if (isFound)
-{
-    Console.WriteLine("Да");
-}
-else
-{
-    Console.WriteLine("Нет");
-}
+Console.WriteLine($"Элемент {numberForSearch}: {isFound} ");
