07152e1664ff7bc578dffb5645241ab17dbc6e9b
Anastasiya <ayrodin@gmail.com>
2024-01-13 15:00:32 +0700

    Решила 2 домашние задачи

--- OneDrive/Рабочий стол/GeekBrains/01_CSHARP/sem3/Program.cs
+++ OneDrive/Рабочий стол/GeekBrains/01_CSHARP/sem3/Program.cs
@@ -93,13 +93,64 @@
 // [2 3 1 7 5 6 3] => [6 18 5] (элемент 7 не имеет пары)
 
 
-int[] array = { 1, -2, 3, 4, 5, -1, -7, -9, 10, -5 };
-int[] result = new int[array.Length / 2];
- // i - индекс 1 элемента, j - индекс послеж=днего элемента
-for (int i = 0, j = array.Length - 1; i < result.Length; i++, j--)
-{
-    result[i] = array[i] * array[j];
-}
- Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
- Console.WriteLine($"Результат: [ {string.Join("; ", result)} ]");
+// int[] array = { 1, 3, 2, 4, 2, 3 };
+// int[] result = new int[array.Length / 2];
+//  // i - индекс 1 элемента, j - индекс послеж=днего элемента
+// for (int i = 0, j = array.Length - 1; i < result.Length; i++, j--)
+// {
+//     result[i] = array[i] * array[j];
+// }
+//  Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+//  Console.WriteLine($"Результат: [ {string.Join("; ", result)} ]");
+
+
+// Задача 1: Задайте одномерный массив из 10 целых 
+// чисел от 1 до 100. Найдите количество элементов 
+// массива, значения которых лежат в отрезке [20,90]. 
+
+// int[] array = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3};
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] >= 10 && array[i] <= 90)
+//     count++;
+// }
+//  return count;
+
+// Console.WriteLine(CountItemsRange(array,10,90));
+
+// Задайте массив на 10 целых чисел. 
+// Напишите программу, которая определяет 
+// количество чётных чисел в массиве.
+
+// int[] array = {2, 5, 2, 3, 6, 3, 7, 8, 2, 4};
+//  int[] a = new int[10];
+//  int count = 0;
+ 
+// for (int i = 0; i < 10; i++)
+// {
+// Console.Write("a[{0}]=", i);
+// a[i] = Convert.ToInt32(Console.ReadLine());
+//  if ((a[i] % 2) == 0)
+//  count++;
+//  }
+//  Console.Write(count);
+//  Console.ReadKey();
+
+
+// int count = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] % 2 == 0)
+//       count++;
+// }
+//      return count; 
+
+
+// arr[i] % 2 == 0
+
+// Console.WriteLine(CountEvenItems(array))
+
+
+
 
