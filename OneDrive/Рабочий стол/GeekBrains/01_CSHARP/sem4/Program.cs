18619ae0dd749a6987029310df104b2068555ef9
Anastasiya <ayrodin@gmail.com>
2024-01-15 15:51:39 +0700

    все задачи семинара

--- OneDrive/Рабочий стол/GeekBrains/01_CSHARP/sem4/Program.cs
+++ OneDrive/Рабочий стол/GeekBrains/01_CSHARP/sem4/Program.cs
@@ -80,19 +80,123 @@
 
 // size = размер, minRange - мин. число в рандоме, 
 // maxRange - макс число в рандоме
-int[] CreateArray(int size, int minRange, int maxRange)
-{
-    int[] array = new int[size]; // Массив на size эл
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(minRange, maxRange + 1);
-    }
-    return array;
-}
+// int[] CreateArray(int size, int minRange, int maxRange)
+// {
+//     int[] array = new int[size]; // Массив на size эл
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(minRange, maxRange + 1);
+//     }
+//     return array;
+// }
 
-Console.Write("Введите размер массива: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int[] array = CreateArray(N, 0, 100);
-// array - массив на N элементов,
-// каждый элемент - число: [0,100]
-Console.WriteLine($"Массив: [ {string.Join(";", array)} ]");
+// Console.Write("Введите размер массива: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int[] array = CreateArray(N, 0, 100);
+// // array - массив на N элементов,
+// // каждый элемент - число: [0,100]
+// Console.WriteLine($"Массив: [ {string.Join(";", array)} ]");
+
+// Задайте массив из N случайных целых чисел (N вводится с
+// клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и
+// делятся нацело на 7.
+// Пример
+// [1 5 11 21 81 4 0 91 2 3]
+// => 2
+
+// // size = размер, minRange - мин. число в рандоме, 
+// // maxRange - макс число в рандоме
+// int[] CreateArray(int size, int minRange, int maxRange)
+// {
+//     int[] array = new int[size]; // Массив на size эл
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(minRange, maxRange + 1);
+//     }
+//     return array;
+// }
+
+// int GetCount(int[] arr)
+// {
+//     int count = 0;
+//     foreach (int number in arr) 
+//     {
+//         if (number % 7 == 0 && number % 10 == 1)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// Console.Write("Введите размер массива: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int[] array = CreateArray(N, 0, 100);
+// // array - массив на N элементов,
+// // каждый элемент - число: [0,100]
+// Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+// Console.WriteLine($"Total: {GetCount(array)}");
+
+// Заполните массив на N (вводится с консоли, не более 8)
+// случайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр из
+// массива. Старший разряд числа находится на 0-м индексе,
+// младший – на последнем.
+// Пример
+// [1 3 2 4 2 3] => 132423
+// [2 3 1] => 231
+
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size]; // Массив на size эл
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0, 10);
+//         // 0, 9
+//     }
+//     return array;
+// }
+// Console.Write("Введите размер массива: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int[] array = CreateArray(N);
+// if (array[0] == 0 || N > 8)
+// {
+//     Console.WriteLine("Размер > 8 эл, или 0 элемент = 0");
+//     return;
+// }
+// Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+// int ConvertArrayToInteger(int[] array)
+// {
+//     int result = 0;
+//     // 294
+//     for (int i = 0, j = array.Length - 1; i < array.Length; i++, j--)
+//     {
+//         result = result + array[i] * (int)Math.Pow(10, j);
+//     }
+//     return result;
+// }
+// Console.WriteLine($"Число: {ConvertArrayToInteger(array)}");
+
+
+// ДЗ
+
+// Задача 1: Напишите программу, которая бесконечно 
+// запрашивает целые числа с консоли. Программа 
+// завершается при вводе символа ‘q’ или при вводе 
+// числа, сумма цифр которого четная.
+
+
+
+// Задача 2: Задайте массив заполненный случайными 
+// трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в 
+// массиве.
+
+
+
+
+// Задача 3: Напишите программу, которая перевернёт 
+// одномерный массив (первый элемент станет 
+// последним, второй – предпоследним и т.д.)
+
